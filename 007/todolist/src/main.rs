cc35113471ed38359f357a81cb021593bd2eb9fb
tb268 <yufuin0731@gmail.com>
2025-11-07 01:34:20 +0900

    007 complete

--- 007/todolist/src/main.rs
+++ 007/todolist/src/main.rs
@@ -1,3 +1,37 @@
+
+#[derive(Debug)]
+struct Todo{
+    title: String,
+    done: bool,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut todos: Vec<Todo>=Vec::new();
+    loop{
+        println!("タスクを入力してください");
+        
+        let mut input = String::new();
+        let _ = std::io::stdin().read_line(&mut input);
+        
+
+
+        if let Some(add_title)=input.trim().strip_prefix("add "){
+
+            let todo= Todo{title: add_title.to_string(), done: false};
+            println!("{:?}を追加しました。",todo.title);
+            todos.push(todo);
+            
+        }else if input.trim()=="list"{
+            println!("todo一覧");
+            for to in &todos{
+
+                println!("{:?}",to);
+            }
+        }else{
+            println!("無効なコマンドです");
+        }
+
+        
+    }
 }
