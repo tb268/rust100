9a033d88e5d47965e5fa3caa6ec3046d7e8bacb1
tb268 <yufuin0731@gmail.com>
2025-11-12 00:35:35 +0900

    013 complete

--- 013/wordcount/src/main.rs
+++ 013/wordcount/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use std::io;
+
+#[derive(Parser,Debug)]
+#[command(version,about,long_about=None)]
+struct Args{
+    #[arg(short,long)]
+    file: String,
+}
+
+
+
+fn main() ->Result<(), io::Error>{
+    let args: Args=Args::parse();
+    let test:String=std::fs::read_to_string( args.file)?;
+    let num=test.split_whitespace().count();
+    println!("num is {}",num);
+    Ok(())
 }
