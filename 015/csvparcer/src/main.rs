7189d30d86756a34ce9aa07b1825d64f6003f200
tb268 <yufuin0731@gmail.com>
2025-11-14 01:20:09 +0900

    015 complete

--- 015/csvparcer/src/main.rs
+++ 015/csvparcer/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use csv::Reader;
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Debug)]
+struct Data{
+    name: String,
+    age: i32,
+    email: String,
+}
+
+fn main()->Result<(), Box<dyn std::error::Error>> {
+
+    let file_path = "data.csv"; 
+    let mut reader = Reader::from_path(file_path)?;
+    for result in reader.deserialize::<Data>(){
+        let data: Data = result?;
+        println!("{:?}",data);
+    }
+    Ok(())
 }
